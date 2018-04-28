72c3934880bb60724f165ae9ccb4b3ea00bee9e5
David Santoro <soulnafein@gmail.com>
2018-04-28 06:13:54 +0000

    Updates

--- src/Messages.elm
+++
@@ -1,9 +0,0 @@
-module Messages exposing (Msg(..))
-
-import Time exposing (Time)
-import Keyboard exposing (KeyCode)
-
-
-type Msg
-    = KeyChanged Bool KeyCode
-    | TimeUpdate Time
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './main.css';
-import { Main } from './Main.elm';
-import charactersPath from './graphics/characters.gif';
-import registerServiceWorker from './registerServiceWorker';
-
-Main.embed(document.getElementById('root'), {charactersPath: charactersPath});
-
-registerServiceWorker();
